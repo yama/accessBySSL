d948ce89dfe0ad7c5b9a1863ce5bc7dd615ca03c
yamamoto <yamamoto@kyms.jp>
2015-10-20 22:44:04 +0900

    Refactor

--- assets/plugins/accessBySSL/accessBySSL.inc.php
+++ assets/plugins/accessBySSL/accessBySSL.inc.php
@@ -19,22 +19,24 @@ class accessBySSL {
     global $modx;
 
     $output = &$modx->documentOutput;
-    $http_host = substr($modx->config['site_url'],strpos($modx->config['site_url'],'://')+3);
-    $http_host = substr($http_host,0,strrpos($http_host,'/'));
+    $host_name = substr($modx->config['site_url'],strpos($modx->config['site_url'],'://')+3);
+    $host_name = rtrim($host_name,'/');
+    $http_site_url = str_replace('https://','http://',$modx->config['site_url']);
 
     foreach ($this->ids as $id) {
       $link = $modx->makeUrl($id, '', '','full'); // Since 1.0.12J
       if(!$link) continue;
       $base_url = trim($modx->config['base_url'],'/');
       $link = $base_url . str_replace($modx->config['site_url'],'',$link);
-      $output = preg_replace("|(https?://{$http_host})?/?{$link}|", "{$this->append}/{$link}", $output);
+      $output = preg_replace("@(https?://{$host_name})?/?{$link}@", "{$this->append}/{$link}", $output);
     }
 
     if (in_array($modx->documentIdentifier, $this->ids)) {
-      $output = preg_replace('/<base href=("|\')(.+)("|\')/', '<base href="' . $this->append . '/"', $output);
+      $output = preg_replace('/<base href=("|\')(.+)\1/', '<base href="' . $this->append . '/"', $output);
       if(strpos($output,'/manager/')!==false) $output = preg_replace("@/manager/@", "manager/", $output);
-      $output = preg_replace('@<a([\s\w\d=\"\']+)href=(\"|\')\/(.+?)(\"|\')@im', "<a$1href=\"http://".$http_host."/$3\"", $output);
-      $output = preg_replace('@<a([\s\w\d=\"\']+)href=\"https?:\/\/".$http_host."\/(https?:\/\/.+)\"@', "<a$1href=\"$2\"", $output);
+      $output = preg_replace('@<a\s+href=("|\')(?!http)/(.+?)\1@im', '<a href="' . $http_site_url . '$2"', $output);
+      $output = preg_replace('@<a\s+href=("|\')(?!http)(.+?)\1@im' , '<a href="' . $http_site_url . '$2"', $output);
+      $output = preg_replace('@<a\s+href=("|\')https://' . $host_name . '(/?)\1@im' , '<a href="http://' . $host_name . '$2"', $output);
     }
     
   }
